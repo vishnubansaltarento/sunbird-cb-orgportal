a3a8c47f22f9edace72af6859f0dfbafa030fe27
Nandini Achutha <nandini.achutha@tarento.com>
2024-06-07 12:25:29 +0530

    Lint fix

--- project/ws/app/src/lib/routes/home/components/user-cards/user-card.component.ts
+++ project/ws/app/src/lib/routes/home/components/user-cards/user-card.component.ts
@@ -616,13 +616,13 @@ export class UserCardComponent implements OnInit, OnChanges {
 
   onSubmit(form: any, user: any, panel: any) {
     if (form.valid) {
-      const dob = this.datePipe.transform(this.updateUserDataForm.controls['dob'].value, 'dd-MM-yyyy')
+      const dobn = this.datePipe.transform(this.updateUserDataForm.controls['dob'].value, 'dd-MM-yyyy')
       this.reqbody = {
         request: {
           userId: user.userId,
           profileDetails: {
             personalDetails: {
-              dob,
+              dob: dobn,
               domicileMedium: this.updateUserDataForm.controls['domicileMedium'].value,
               gender: this.updateUserDataForm.controls['gender'].value,
               category: this.updateUserDataForm.controls['category'].value,
