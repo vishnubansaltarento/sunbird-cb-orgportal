9ef09600e86beb2e35429c13c24c7ea9648e7dfa
Nandini Achutha <nandini.achutha@tarento.com>
2024-06-14 11:09:06 +0530

    Fixed 4793,4792

--- project/ws/app/src/lib/routes/home/components/user-cards/user-card.component.ts
+++ project/ws/app/src/lib/routes/home/components/user-cards/user-card.component.ts
@@ -62,7 +62,7 @@ export class UserCardComponent implements OnInit, OnChanges {
   lastIndex = 20
   pageSize = 20
 
-  userStatus: any
+  // userStatus: any
   rolesList: any = []
   rolesObject: any = []
   uniqueRoles: any = []
@@ -404,32 +404,34 @@ export class UserCardComponent implements OnInit, OnChanges {
     })
   }
 
-  getUerData(user: any, data: any, panel: any) {
-    if (panel.expanded) {
+  getApprovalUserData(user: any, data: any, openPanel: MatExpansionPanel) {
+    if (openPanel.expanded) {
+      user.enableEdit = false
+      this.approveUserDataForm.reset()
+      user.needApprovalList = []
+      this.actionList = []
+      this.comment = ''
+      this.getApprovalList(data)
+    }
+  }
+
+  getUerData(user: any, openPanel: MatExpansionPanel, index: any) {
+    if (openPanel.expanded) {
       user.enableEdit = false
       let profileDataAll = user
-      this.userStatus = profileDataAll.isDeleted ? 'Inactive' : 'Active'
 
       const profileData = profileDataAll.profileDetails
       this.updateTags(profileData)
 
-      if (this.isApprovals) {
-        this.approveUserDataForm.reset()
-        user.needApprovalList = []
-        this.actionList = []
-        this.comment = ''
-        this.getApprovalList(data)
-      } else {
-        this.usersSvc.getUserById(user.userId).subscribe((res: any) => {
-          if (res) {
-            profileDataAll = res
-            profileDataAll.enableEdit = false
-            // user = profileDataAll
-            this.userRoles.clear()
-            this.mapRoles(profileDataAll)
-          }
-        })
-      }
+      this.usersSvc.getUserById(user.userId).subscribe((res: any) => {
+        if (res) {
+          user = res
+          user.enableEdit = false
+          this.userRoles.clear()
+          this.mapRoles(user)
+          this.usersData[index] = user
+        }
+      })
     }
   }
 
@@ -632,16 +634,18 @@ export class UserCardComponent implements OnInit, OnChanges {
           userId: user.userId,
           profileDetails: {
             personalDetails: {
-              dob: dobn,
-              domicileMedium: this.updateUserDataForm.controls['domicileMedium'].value,
-              gender: this.updateUserDataForm.controls['gender'].value,
-              category: this.updateUserDataForm.controls['category'].value,
+              dob: dobn ? dobn : '',
+              domicileMedium: this.updateUserDataForm.controls['domicileMedium'].value ?
+                this.updateUserDataForm.controls['domicileMedium'].value : '',
+              gender: this.updateUserDataForm.controls['gender'].value ? this.updateUserDataForm.controls['gender'].value : '',
+              category: this.updateUserDataForm.controls['category'].value ? this.updateUserDataForm.controls['category'].value : '',
               mobile: this.updateUserDataForm.controls['mobile'].value,
               primaryEmail: this.updateUserDataForm.controls['primaryEmail'].value,
             },
             professionalDetails: [
               {
-                designation: this.updateUserDataForm.controls['designation'].value,
+                designation: this.updateUserDataForm.controls['designation'].value ?
+                  this.updateUserDataForm.controls['designation'].value : '',
                 group: this.updateUserDataForm.controls['group'].value,
               },
             ],
@@ -649,8 +653,10 @@ export class UserCardComponent implements OnInit, OnChanges {
               tag: this.selectedtags,
             },
             employmentDetails: {
-              pinCode: this.updateUserDataForm.controls['pincode'].value,
-              employeeCode: this.updateUserDataForm.controls['employeeID'].value,
+              pinCode: this.updateUserDataForm.controls['pincode'].value ?
+                this.updateUserDataForm.controls['pincode'].value : '',
+              employeeCode: this.updateUserDataForm.controls['employeeID'].value ?
+                this.updateUserDataForm.controls['employeeID'].value : '',
             },
           },
         },
