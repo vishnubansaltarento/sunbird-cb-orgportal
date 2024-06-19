ac1d049bb99ff12488685f32466866e256068cce
sohail amjad <sohail.amjad@tarento.com>
2024-06-19 16:38:56 +0530

    tslint issues

--- project/ws/app/src/lib/routes/home/components/request-list/request-list.component.ts
+++ project/ws/app/src/lib/routes/home/components/request-list/request-list.component.ts
@@ -8,6 +8,9 @@ import { MatDialog, MatSnackBar, MatTableDataSource } from '@angular/material'
 import { ConfirmationBoxComponent } from '../../../training-plan/components/confirmation-box/confirmation.box.component'
 import { AssignListPopupComponent } from './assign-list-popup/assign-list-popup.component'
 import { LoaderService } from '../../../../../../../../../src/app/services/loader.service'
+/* tslint:disable */
+import _ from 'lodash'
+/* tslint:enable */
 export enum statusValue {
   Assigned= 'Assigned',
   Unassigned = 'Unassigned',
@@ -54,10 +57,11 @@ export class RequestListComponent implements OnInit {
   invalidRes: any
   detailsEvent: any
   dataSource: any
-  displayedColumns: string[] = ['RequestId', 'title','requestor', 'requestType',
+  displayedColumns: string[] = ['RequestId', 'title', 'requestor', 'requestType',
    'requestStatus', 'assignee', 'requestedOn', 'interests', 'action']
   statusKey = statusValue
-
+  fullProfile: any
+ rootOrgId: any
   constructor(private sanitizer: DomSanitizer,
               private homeService: ProfileV2Service,
               private datePipe: DatePipe,
@@ -76,6 +80,10 @@ export class RequestListComponent implements OnInit {
 
   ngOnInit() {
     this.configSvc = this.activeRoute.snapshot.data['configService']
+    this.fullProfile = _.get(this.activeRoute.snapshot, 'data.configService')
+    if (this.fullProfile.userProfile && this.fullProfile.userProfile.rootOrgId) {
+      this.rootOrgId = this.fullProfile.userProfile.rootOrgId
+    }
     this.getRequestList()
     this.pageConfig = this.activeRoute.snapshot.data['pageData']
     this.hasAccess()
@@ -251,7 +259,9 @@ export class RequestListComponent implements OnInit {
   getRequestList() {
     this.loaderService.changeLoaderState(true)
     const request = {
-        filterCriteriaMap: {},
+        filterCriteriaMap: {
+          rootOrgId: this.rootOrgId,
+        },
         requestedFields: [],
         facets: [],
         pageNumber: this.pageNo,
@@ -260,7 +270,7 @@ export class RequestListComponent implements OnInit {
         orderDirection: 'ASC',
     }
     this.homeService.getRequestList(request).subscribe(res => {
-      if(res){
+      if (res) {
       this.requestListData = res.data
       if (this.requestListData) {
         this.loaderService.changeLoaderState(false)
