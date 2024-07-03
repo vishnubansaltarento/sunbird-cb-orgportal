f40e9177394e4802482c0836f1d16a762f5c9d0d
sohail amjad <sohail.amjad@tarento.com>
2024-07-03 16:18:41 +0530

    pointer events issue resolved

--- project/ws/app/src/lib/routes/home/components/request-list/request-list.component.ts
+++ project/ws/app/src/lib/routes/home/components/request-list/request-list.component.ts
@@ -107,6 +107,14 @@ export class RequestListComponent implements OnInit {
 
 }
 
+getPointerEventsStyle(element:any){
+  return {
+   'pointer-events': (element.status !== this.statusKey.Inprogress && 
+     element.status !== this.statusKey.invalid && 
+     element.status !== this.statusKey.fullfill) ? 'auto' : 'none',
+  }
+ }
+
   hasAccess() {
     let flag = false
     if (this.pageConfig && this.pageConfig.data && this.pageConfig.data.actionMenu) {
